2adb4b30d7af1ae5c9658620d4899467999c8e77
Aman Raj <56683921+amanrajgp@users.noreply.github.com>
2024-05-17 17:26:21 +0530

    Update app.py
    add camera feature

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import time
+import io
 import pytesseract as tess
 from PIL import Image
 import streamlit as st
@@ -10,31 +11,35 @@ import asyncio
 
 # Configure Google Generative AI
 google_api_key = "AIzaSyBoxsa2ARTumKvRL5wLEfRoKm4Xc6zoKq0"
+# genai.configure(api_key=google_api_key)
 
-# Ensure pytesseract can find the tesseract executable
-# This step might not be necessary on Streamlit Cloud if tesseract is correctly installed and in the PATH
-tess.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
+# Setup the Tesseract executable path
+tess.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 # Create the Google Generative AI model
 # Note: Ensure this uses a synchronous approach
 
+
+# Define the main function
+
+
 def main():
     st.title("AskMe")
 
-    image_path = st.file_uploader("Upload an Image", type=["png", "jpg", "jpeg"])
     question = st.text_input("Ask Question:")
-    
+    img_file_buffer = st.camera_input("Take a picture")
 
-    def gettext(image_path):
-        try:
-            img = Image.open(image_path)
+    def gettext(img_file_buffer):
+        if img_file_buffer is not None:
+            bytes_data = img_file_buffer.getvalue()
+            img = Image.open(io.BytesIO(bytes_data))  # Convert bytes to PIL Image
             text = tess.image_to_string(img)
             return text
-        except tess.TesseractNotFoundError:
-            st.error("Tesseract OCR is not installed or found.")
-            return ""
+        else:
+            return None 
 
     async def generate_response(text, question):
+
         prompt = ChatPromptTemplate.from_template("""
             <s>[INSTRUCT]
             Role: As a contract generation specialist, your primary function is to analyze the provided Text {text} and answer the question {question} given by user.
@@ -49,7 +54,8 @@ def main():
             * Do not provide any information if you are not able to find.
             [/INSTRUCT]</s>
         """)
-        model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.8, google_api_key=google_api_key)
+        model = ChatGoogleGenerativeAI(
+            model="gemini-pro", temperature=0.8, google_api_key=google_api_key)
         output_parser = StrOutputParser()
         chain = prompt | model | output_parser
 
@@ -57,22 +63,22 @@ def main():
         return response
 
     if st.button("Go"):
-        if question and image_path:
+
+        if question and img_file_buffer:
             st.success("Generating...")
-            text = gettext(image_path)
+            text = gettext(img_file_buffer)
 
-            if text:
-                # Run the asynchronous function and wait for the result
-                loop = asyncio.new_event_loop()
-                asyncio.set_event_loop(loop)
-                response = loop.run_until_complete(generate_response(text, question))
+            # Run the asynchronous function and wait for the result
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            response = loop.run_until_complete(
+                generate_response(text, question))
 
-                st.write("**Generated Response:**")
-                st.write(response)
-            else:
-                st.error("Failed to extract text from the image.")
+            st.write("**Generated Response:**")
+            st.write(response)
         else:
             st.error("Please provide both a question and an image.")
+
 
 # Run the main function
 if __name__ == "__main__":
